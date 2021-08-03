1b74985c9a6bc7f6e028406020166547c3dc578d
Tom Roddick <tomroddick@gmail.com>
2021-08-03 23:43:01 +0100

    first

---
+++ trial.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy
+
+
+# In[3]:
+
+
+import pandas
+
+
+# In[4]:
+
+
+df1 = []
+
+
+# In[5]:
+
+
+print("3")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
